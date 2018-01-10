44ff8f020de1604385d908cbd137e632125a3ede
Cristian Vega <cristian.vega08@gmail.com>
2018-01-09 23:14:02 -0500

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,52 @@
 const path = require('path');
-const express = require('express');
+const http = require('http')
+const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server)
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket)=> {  // create a socket connection
+  console.log('New client connected')
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Wellcome to the chat room',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'Cristian joined',
+    createdAt: new Date().getTime()
+  })
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client was disconnected')
+  })
+})
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
